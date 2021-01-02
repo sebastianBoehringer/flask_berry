ad53078748734650bc259242a82c5f58b8401f86
sebastianBoehringer <45266845+sebastianBoehringer@users.noreply.github.com>
2021-01-02 15:38:46 +0100

    tests for saving/updating labels

--- tests/test_labels.py
+++ tests/test_labels.py
@@ -14,3 +14,26 @@ def test_load_non_existent_label_from_db_returns_none(app: Flask):
     with app.test_request_context():
         label = Label.from_db(234)
         assert label is None, "A label with id 234 was found: {0}".format(label)
+
+
+def test_can_save_label(app: Flask):
+    with app.test_request_context():
+        label = Label(name="Test Save")
+        label.save()
+        assert label.id is not None, "label did not have an id after being saved"
+        control_label = Label.from_db(label.id)
+        assert control_label is not None, "label with id {0} does not exist".format(label.id)
+        assert "Test Save" in control_label.name
+
+
+def test_can_update_label(app: Flask):
+    with app.test_request_context():
+        label = Label(name="to change")
+        label.save()
+        label.name = "Something else"
+        label.update()
+
+        control_label = Label.from_db(label.id)
+        assert control_label is not None, "could not find a label with the given id {0}".format(label.id)
+        assert "Something else" in control_label.name, "label name [{0}] does not match expected ['Something else']". \
+            format(label)
